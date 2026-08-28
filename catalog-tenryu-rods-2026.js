0fce6d6c714a6616800de0ed4d46b2ce58813793
48wr9f4wgp-lab <48wr9f4wgp@privaterelay.appleid.com>
2026-08-29 08:39:21 +0900

    Add TENRYU official rod research batch

---
+++ catalog-tenryu-rods-2026.js
@@ -0,0 +1,57 @@
+(()=>{
+  const R='2026-08-29';
+  const G=[
+    ['Rayz Spectra','https://fishing.tenryu-magna.com/trout/spectra.html',[
+      ['RZS472S-UL',1.40,"1.40m [4'7\"]",2,'2',59,'UL','UL','1-6g',1,6,'Mono 1-4lb / PE 0.2-0.6号',0.2,0.6,'023595'],
+      ['RZS4112S-LL',1.50,"1.50m [4'11\"]",2,'2',62,'','LL','1-8g',1,8,'Mono 1-5lb / PE 0.2-0.6号',0.2,0.6,'023601'],
+      ['RZS512S-LML',1.55,"1.55m [5'1\"]",2,'2',65,'','LML','2-10g',2,10,'Mono 3-6lb / PE 0.4-0.8号',0.4,0.8,'023618'],
+      ['RZS652S-LML',1.96,"1.96m [6'5\"]",2,'2',77,'','LML','2-12g',2,12,'Mono 3-6lb / PE 0.4-0.8号',0.4,0.8,'023625'],
+      ['RZS712S-ML',2.16,"2.16m [7'1\"]",2,'2',86,'ML','ML','3-18g',3,18,'Mono 3-8lb / PE 0.4-0.8号',0.4,0.8,'023632'],
+      ['RZS772S-MH',2.31,"2.31m [7'7\"]",2,'2',112,'MH','MH','6-25g',6,25,'Mono 6-14lb / PE 0.6-1.2号',0.6,1.2,'023649'],
+      ['RZS872S-H',2.62,"2.62m [8'7\"]",2,'2',140,'H','H','7-30g',7,30,'Mono 8-16lb / PE 0.6-1.2号',0.6,1.2,'023656'],
+      ['RZS472B-UL',1.40,"1.40m [4'7\"]",2,'2',68,'UL','UL','1-6g',1,6,'Mono 1-4lb / PE 0.2-0.6号',0.2,0.6,'023663'],
+      ['RZS4112B-LL',1.50,"1.50m [4'11\"]",2,'2',71,'','LL','1-8g',1,8,'Mono 1-5lb / PE 0.2-0.6号',0.2,0.6,'023670']
+    ]],
+    ['JIG-ZAM Dragg Force','https://fishing.tenryu-magna.com/offshore/jig-zam_draggforce.html',[
+      ['JDF631S-2/3',1.90,"1.90m [6'3\"]",1,'1',189,'','','100-180g (Best100-120g)',100,180,'MAX PE 4.0号',null,4,'022819'],
+      ['JDF621S-3/4',1.88,"1.88m [6'2\"]",1,'1',198,'','','120-200g (Best120-150g)',120,200,'MAX PE 4.0号',null,4,'022826'],
+      ['JDF611S-4/5',1.86,"1.86m [6'1\"]",1,'1',206,'','','120-230g (Best150-180g)',120,230,'MAX PE 4.0号',null,4,'022833'],
+      ['JDF601S-5/6',1.83,"1.83m [6'0\"]",1,'1',213,'','','150-250g (Best180-200g)',150,250,'MAX PE 5.0号',null,5,'022840'],
+      ['JDF591B-G5/6',1.75,"1.75m [5'9\"]",1,'1',240,'','','MAX350g (Best150-300g)',null,350,'MAX PE 5.0号',null,5,'022949'],
+      ['JDF591B-G7/8',1.75,"1.75m [5'9\"]",1,'1',258,'','','MAX500g (Best250-400g)',null,500,'MAX PE 6.0号',null,6,'022956']
+    ]],
+    ['HORIZON MJ','https://fishing.tenryu-magna.com/offshore/horizon_mj.html',[
+      ['HMJ5101B-L',1.78,"1.78m [5'10\"]",1,'1',110,'L','L','High60-120g / Slow100-250g',null,null,'MAX PE 2号',null,2,'022000'],
+      ['HMJ5101B-ML',1.78,"1.78m [5'10\"]",1,'1',115,'ML','ML','High80-150g / Slow120-300g',null,null,'MAX PE 2号',null,2,'022017'],
+      ['HMJ5101B-M',1.78,"1.78m [5'10\"]",1,'1',120,'M','M','High100-180g / Slow150-350g',null,null,'MAX PE 2.5号',null,2.5,'022024'],
+      ['HMJ5101B-MH',1.78,"1.78m [5'10\"]",1,'1',126,'MH','MH','High120-200g / Slow180-400g',null,null,'MAX PE 3号',null,3,'022031'],
+      ['HMJ5101B-H',1.78,"1.78m [5'10\"]",1,'1',131,'H','H','High150-250g / Slow200-450g',null,null,'MAX PE 3号',null,3,'022048'],
+      ['HMJ642B-LL',1.93,"1.93m [6'4\"]",2,'2*',111,'','LL','High40-100g / Slow80-200g',null,null,'MAX PE 2号',null,2,'023175'],
+      ['HMJ642B-L',1.93,"1.93m [6'4\"]",2,'2*',114,'L','L','High60-120g / Slow100-250g',null,null,'MAX PE 2号',null,2,'023182'],
+      ['HMJ642B-ML',1.93,"1.93m [6'4\"]",2,'2*',119,'ML','ML','High80-150g / Slow120-300g',null,null,'MAX PE 2号',null,2,'023199'],
+      ['HMJ642B-M',1.93,"1.93m [6'4\"]",2,'2*',121,'M','M','High100-180g / Slow150-350g',null,null,'MAX PE 2.5号',null,2.5,'023205'],
+      ['HMJ642B-MH',1.93,"1.93m [6'4\"]",2,'2*',128,'MH','MH','High120-200g / Slow180-400g',null,null,'MAX PE 3号',null,3,'023212'],
+      ['HMJ642B-H',1.93,"1.93m [6'4\"]",2,'2*',131,'H','H','High150-250g / Slow200-450g',null,null,'MAX PE 3号',null,3,'023229']
+    ]],
+    ['Red Flip','https://fishing.tenryu-magna.com/offshore/redflip.html',[
+      ['RF5101B-UL/VSN',1.78,"1.78m [5'10\"]",1,'1',96,'UL','UL','Vertical 30-100g / Dotera MAX150g',null,null,'MAX PE 1.0号',null,1,'022321'],
+      ['RF5101B-ML/VSK',1.78,"1.78m [5'10\"]",1,'1',100,'ML','ML','Vertical 45-160g / Dotera MAX200g',null,null,'MAX PE 1.2号',null,1.2,'022338'],
+      ['RF642B-UL/VDN',1.93,"1.93m [6'4\"]",2,'2*',105,'UL','UL','Vertical 30-100g / Dotera MAX150g',null,null,'MAX PE 1.0号',null,1,'023281'],
+      ['RF642B-L/VDN',1.93,"1.93m [6'4\"]",2,'2*',113,'L','L','Vertical 45-120g / Dotera MAX180g',null,null,'MAX PE 1.0号',null,1,'022345'],
+      ['RF642B-M/VDK',1.93,"1.93m [6'4\"]",2,'2*',116,'M','M','Vertical 60-180g / Dotera MAX250g',null,null,'MAX PE 1.2号',null,1.2,'022352'],
+      ['RF6102B-L/DN',2.08,"2.08m [6'10\"]",2,'2*',117,'L','L','Vertical 45-140g / Dotera MAX250g',null,null,'MAX PE 1.2号',null,1.2,'023298'],
+      ['RF6102B-MH/DK',2.08,"2.08m [6'10\"]",2,'2*',129,'MH','MH','Vertical 60-200g / Dotera MAX300g',null,null,'MAX PE 1.5号',null,1.5,'022369'],
+      ['RF772B-L/LN',2.31,"2.31m [7'7\"]",null,'1 & H',126,'L','L','Vertical 45-140g / Dotera MAX250g',null,null,'MAX PE 1.2号',null,1.2,'023328'],
+      ['RF652S-L/CN',1.96,"1.96m [6'5\"]",2,'2*',100,'L','L','Vertical 45-120g / Dotera MAX180g / Cast MAX60g',null,null,'MAX PE 1.0号',null,1,'023304'],
+      ['RF672S-ML/CK',2.00,"2.00m [6'7\"]",2,'2*',101,'ML','ML','Vertical 45-160g / Dotera MAX200g / Cast MAX75g',null,null,'MAX PE 1.2号',null,1.2,'022376']
+    ]]
+  ];
+  const rows=[];
+  for(const [series,url,list] of G)for(const [model,length_m,length_raw,pieces,pieces_raw,weight_g,power,power_raw,lure_weight_raw,lure_min_g,lure_max_g,line_weight_raw,line_pe_min,line_pe_max,product_code] of list)rows.push({
+    maker:'TENRYU',category:'rod',series,generation:series==='Rayz Spectra'?'2026':'current',model,display_name:`${series} ${model}`,status:'current',
+    specs:{length_ft:null,length_m,length_raw,pieces,pieces_raw,weight_g,power,power_raw,lure_min_g,lure_max_g,jig_max_g:null,lure_weight_raw,line_pe_min,line_pe_max,line_weight_raw,sinker_load_raw:''},
+    source:{source_type:'manufacturer_official',source_provider:'tenryu-official-research',source_url:url,retrieved_at:R,last_verified:R,license_status:'restricted'},identifiers:{product_code}
+  });
+  const batch=Object.freeze({id:'tenryu-rods-2026',rows:Object.freeze(rows.map(r=>Object.freeze({...r,specs:Object.freeze({...r.specs}),source:Object.freeze({...r.source}),identifiers:Object.freeze({...r.identifiers})})))});
+  const reg=globalThis.FISH_TARGET_CATALOG_BATCH_ROWS||(globalThis.FISH_TARGET_CATALOG_BATCH_ROWS=[]);if(reg.some(x=>x?.id===batch.id))throw new Error('Duplicate catalog batch id: '+batch.id);reg.push(batch);
+})();
